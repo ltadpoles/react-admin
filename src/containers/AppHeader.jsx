79a22b914a0c149b3d19ca37c53ed0ba24b90850
tadpole <ltadpole@163.com>
2019-10-09 18:40:35 +0800

    完善头部

--- src/containers/AppHeader.jsx
+++ src/containers/AppHeader.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 import '../style/containers/app-header.scss'
 import baseAvatar from '../assets/images/user.jpg'
-import { Icon } from 'antd'
+import { Menu, Dropdown, Icon } from 'antd'
+
+const menu = (
+    <Menu>
+        <Menu.Item>个人信息</Menu.Item>
+        <Menu.Item>系统设置</Menu.Item>
+        <Menu.Item><Icon type="logout" />退出登录</Menu.Item>
+    </Menu>
+)
 
 class AppHeader extends Component {
     render() { 
@@ -12,7 +20,12 @@ class AppHeader extends Component {
                     <Icon style={{fontSize: '2rem'}} onClick={menuClick} type={menuToggle ? "menu-unfold" : "menu-fold"} />
                 </div>
                 <div className="right">
-                    <img src={avatar ? avatar : baseAvatar} alt='头像'></img>
+                    {/* <div><Icon style={{fontSize: '2rem', marginRight: '2rem'}} type="sound" /></div> */}
+                    <Dropdown overlay={menu}>
+                        <div className="ant-dropdown-link">
+                            <img src={avatar ? avatar : baseAvatar} alt='头像'></img>
+                        </div>
+                    </Dropdown>
                 </div> 
             </header>
         );
