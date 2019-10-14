785f6f46fddfb15e43182027e5d6486b58464c98
tadpole <ltadpole@163.com>
2019-10-14 22:24:03 +0800

    Other

--- src/views/Others/Animation/Animation.jsx
+++ src/views/Others/Animation/Animation.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
-import { Layout, Divider, Row, Col, Button } from 'antd'
+import { Layout, Divider, Row, Col, Button, Tabs } from 'antd'
 import CustomBreadcrumb from '../../../components/CustomBreadcrumb'
+import '../../../style/view-style/animation.scss'
 import 'animate.css'
 
-const animationType = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'headShake', 'swing', 'tada']
+const { TabPane } = Tabs
+const typeIn = ['bounceInDown', 'bounceInLeft', 'bounceInRight', 'bounceInUp', 'fadeIn', 'fadeInDown', 'fadeInLeft', 'fadeInLeftBig', 'fadeInRight', 'fadeInRightBig', 'fadeInUp', 'fadeInUpBig', 'flipInX', 'flipInY', 'zoomIn', 'rotateIn']
+const typeOut = ['bounceOut', 'bounceOutDown', 'bounceOutLeft', 'bounceOutRight', 'bounceOutUp', 'fadeInDown', 'fadeOut', 'fadeOutDown', 'fadeOutDownBig', 'fadeOutLeft', 'fadeOutLeftBig', 'fadeOutRight', 'fadeOutRightBig', 'fadeOutUp', 'fadeOutUpBig', 'rotateOut']
+const typeOther = ['bounceIn', 'bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'headShake', 'swing', 'tada', 'wobble', 'jello']
 
 class AnimationView extends Component {
     constructor(props) {
@@ -29,20 +33,40 @@ class AnimationView extends Component {
                     <Divider />
                     <p>当页面需要动态效果时。</p>
                 </div>
-                <Row>
-                    <Col>
+                <Row gutter={8}>
+                    <Col span={10}>
                         <div className="base-style">
-                            <Divider orientation='left'>进场</Divider>
-                            {
-                                animationType.map((v, i) => (
-                                    <Button key={i} onClick={this.changeType.bind(this, v)}>{v}</Button>
-                                ))
-                            }
-                            <div style={{ fontSize: '4.8rem', textAlign: 'center', padding: '3rem'}} className={this.state.fontType}>
+                            <Tabs type="card">
+                                <TabPane tab="进场" key="1">
+                                    {
+                                        typeIn.map((v, i) => (
+                                            <Button type="primary" size='small' key={i} onClick={this.changeType.bind(this, v)}>{v}</Button>
+                                        ))
+                                    }
+                                </TabPane>
+                                <TabPane tab="出场" key="2">
+                                    {
+                                        typeOut.map((v, i) => (
+                                            <Button type="primary" size='small' key={i} onClick={this.changeType.bind(this, v)}>{v}</Button>
+                                        ))
+                                    }
+                                </TabPane>
+                                <TabPane tab="其它" key="3">
+                                    {
+                                        typeOther.map((v, i) => (
+                                            <Button type="primary" size='small' key={i} onClick={this.changeType.bind(this, v)}>{v}</Button>
+                                        ))
+                                    }
+                                </TabPane>
+                            </Tabs>
+                        </div>
+                    </Col>
+                    <Col span={14}>
+                        <div className="base-style">
+                            <div style={{ fontSize: '4.8rem', textAlign: 'center', padding: '3rem' }} className={this.state.fontType}>
                                 Animate.css
                             </div>
                         </div>
-
                     </Col>
                 </Row>
             </Layout>
