7d458dfda1f5dad2d70e2f450653baa3e6078ef4
tadpole <ltadpole@163.com>
2019-10-22 11:18:37 +0800

    font

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import loadable from './utils/loadable'
 import 'animate.css'
-import './style/App.scss'
 import './style/base.scss'
+import './style/App.scss'
 
 // 公共模块
 const DefaultLayout = loadable(() => import('./containers'))
@@ -16,11 +16,11 @@ const Login = loadable(() => import('./views/Login'))
 const App = props => (
     <Router>
         <Switch>
-            <Route path="/" exact render={() => <Redirect to="/index" />} />
-            <Route path="/500" component={View500} />
-            <Route path="/login" component={Login} />
-            <Route path="/404" component={View404} />
-            <Route path="/" component={DefaultLayout} />
+            <Route path='/' exact render={() => <Redirect to='/index' />} />
+            <Route path='/500' component={View500} />
+            <Route path='/login' component={Login} />
+            <Route path='/404' component={View404} />
+            <Route path='/' component={DefaultLayout} />
         </Switch>
     </Router>
 )
