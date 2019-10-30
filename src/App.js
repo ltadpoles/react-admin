dcf8b0e650b916d7f055dc668cbb120ca8d13430
tadpole <ltadpole@163.com>
2019-10-30 23:21:04 +0800

    webpack别名

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import './style/base.scss'
 import './style/App.scss'
 
 // 公共模块
-const DefaultLayout = loadable(() => import('./containers'))
+const DefaultLayout = loadable(() => import(/* webpackChunkName: 'default' */'./containers'))
 
 // 基础页面
-const View404 = loadable(() => import('./views/Others/404'))
-const View500 = loadable(() => import('./views/Others/500'))
-const Login = loadable(() => import('./views/Login'))
+const View404 = loadable(() => import(/* webpackChunkName: '404' */'./views/Others/404'))
+const View500 = loadable(() => import(/* webpackChunkName: '500' */'./views/Others/500'))
+const Login = loadable(() => import(/* webpackChunkName: 'login' */'./views/Login'))
 
 const App = props => (
     <Router>
@@ -20,7 +20,7 @@ const App = props => (
             <Route path='/500' component={View500} />
             <Route path='/login' component={Login} />
             <Route path='/404' component={View404} />
-            <Route path='/' component={DefaultLayout} />
+            <Route component={DefaultLayout} />
         </Switch>
     </Router>
 )
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,34 +1,34 @@
 import loadable from '../utils/loadable'
 
-const Index = loadable(() => import('../views/Index'))
+const Index = loadable(() => import(/* webpackChunkName: 'index' */'../views/Index'))
 
 // 通用
-const ButtonView = loadable(() => import('../views/PublicView/Button'))
-const IconView = loadable(() => import('../views/PublicView/Icon'))
+const ButtonView = loadable(() => import(/* webpackChunkName: 'button' */'../views/PublicView/Button'))
+const IconView = loadable(() => import(/* webpackChunkName: 'icon' */'../views/PublicView/Icon'))
 
 // 导航
-const DropdownView = loadable(() => import('../views/NavView/Dropdown'))
-const MenuView = loadable(() => import('../views/NavView/Menu'))
-const StepView = loadable(() => import('../views/NavView/Step'))
+const DropdownView = loadable(() => import(/* webpackChunkName: 'dropdown' */'../views/NavView/Dropdown'))
+const MenuView = loadable(() => import(/* webpackChunkName: 'menu' */'../views/NavView/Menu'))
+const StepView = loadable(() => import(/* webpackChunkName: 'step' */'../views/NavView/Step'))
 
 // 表单
-const FormBaseView = loadable(() => import('../views/FormView/FormBaseView'))
-const FormStepView = loadable(() => import('../views/FormView/FormStepView'))
+const FormBaseView = loadable(() => import(/* webpackChunkName: 'formBase' */'../views/FormView/FormBaseView'))
+const FormStepView = loadable(() => import(/* webpackChunkName: 'formStep' */'../views/FormView/FormStepView'))
 
 // 展示
-const TableView = loadable(() => import('../views/ShowView/Table'))
-const CollapseView = loadable(() => import('../views/ShowView/Collapse'))
-const TreeView = loadable(() => import('../views/ShowView/Tree'))
-const TabsView = loadable(() => import('../views/ShowView/Tabs'))
+const TableView = loadable(() => import(/* webpackChunkName: 'table' */'../views/ShowView/Table'))
+const CollapseView = loadable(() => import(/* webpackChunkName: 'collapse' */'../views/ShowView/Collapse'))
+const TreeView = loadable(() => import(/* webpackChunkName: 'tree' */'../views/ShowView/Tree'))
+const TabsView = loadable(() => import(/* webpackChunkName: 'tabs' */'../views/ShowView/Tabs'))
 
 // 其它
-const ProgressView = loadable(() => import('../views/Others/Progress'))
-const AnimationView = loadable(() => import('../views/Others/Animation'))
-const EditorView = loadable(() => import('../views/Others/Editor'))
-const UploadView = loadable(() => import('../views/Others/Upload'))
+const ProgressView = loadable(() => import(/* webpackChunkName: 'progress' */'../views/Others/Progress'))
+const AnimationView = loadable(() => import(/* webpackChunkName: 'animation' */'../views/Others/Animation'))
+const EditorView = loadable(() => import(/* webpackChunkName: 'editor' */'../views/Others/Editor'))
+const UploadView = loadable(() => import(/* webpackChunkName: 'upload' */'../views/Others/Upload'))
 
-const Three = loadable(() => import('../views/TestView'))
-const About = loadable(() => import('../views/About'))
+const Three = loadable(() => import(/* webpackChunkName: 'three' */'../views/TestView'))
+const About = loadable(() => import(/* webpackChunkName: 'about' */'../views/About'))
 
 const routes = [
     { path: '/index', exact: true, name: 'Index', component: Index, auth: [1] },
@@ -40,7 +40,7 @@ const routes = [
     { path: '/form/base-form', exact: false, name: '表单', component: FormBaseView },
     { path: '/form/step-form', exact: false, name: '表单', component: FormStepView },
     { path: '/show/table', exact: false, name: '表格', component: TableView },
-    // { path: '/show/collapse', exact: false, name: '折叠面板', component: CollapseView },
+    { path: '/show/collapse', exact: false, name: '折叠面板', component: CollapseView },
     { path: '/show/tree', exact: false, name: '树形控件', component: TreeView },
     { path: '/show/tabs', exact: false, name: '标签页', component: TabsView },
     { path: '/others/progress', exact: false, name: '进度条', component: ProgressView, auth: [1] },
