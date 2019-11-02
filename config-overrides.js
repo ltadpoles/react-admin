1a26f0380041bbbaba325f821e199d1e5bdb294f
tadpole <ltadpole@163.com>
2019-11-02 20:32:38 +0800

    config

--- config-overrides.js
+++ config-overrides.js
@@ -9,5 +9,29 @@ module.exports = override(
     }),
     addWebpackAlias({
         ['@']: path.resolve(__dirname, 'src')
-    })
+    }),
+    (config, env) => {
+        config.optimization.splitChunks = {
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    minSize: 30000,
+                    minChunks: 1,
+                    chunks: 'initial',
+                    priority: 1 // 设置处理的优先级，数值越大越优先处理
+                },
+                commons: {
+                    test: /[\\/]src[\\/]common[\\/]/,
+                    name: 'commons',
+                    minSize: 30000,
+                    minChunks: 3,
+                    chunks: 'initial',
+                    priority: -1,
+                    reuseExistingChunk: true // 允许使用已经存在的代码块
+                }
+            }
+        }
+        return config
+    }
 )
