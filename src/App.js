362c1bd0a40b3fdf97004c3ac8e970630c90af8f
tadpole <ltadpole@163.com>
2019-11-09 00:52:21 +0800

    hook

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import './style/base.scss'
 import './style/App.scss'
 
 // 公共模块
-const DefaultLayout = loadable(() => import(/* webpackChunkName: 'default' */'./containers'))
+const DefaultLayout = loadable(() => import(/* webpackChunkName: 'default' */ './containers'))
 
 // 基础页面
-const View404 = loadable(() => import(/* webpackChunkName: '404' */'./views/Others/404'))
-const View500 = loadable(() => import(/* webpackChunkName: '500' */'./views/Others/500'))
-const Login = loadable(() => import(/* webpackChunkName: 'login' */'./views/Login'))
+const View404 = loadable(() => import(/* webpackChunkName: '404' */ './views/Others/404'))
+const View500 = loadable(() => import(/* webpackChunkName: '500' */ './views/Others/500'))
+const Login = loadable(() => import(/* webpackChunkName: 'login' */ './views/Login'))
 
 const App = props => (
     <Router>
--- src/views/Others/Animation/Animation.jsx
+++ src/views/Others/Animation/Animation.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Layout, Divider, Row, Col, Button, Tabs } from 'antd'
 import CustomBreadcrumb from '@/components/CustomBreadcrumb'
 import '@/style/view-style/animation.scss'
@@ -53,79 +53,67 @@ const typeOther = [
     'jello'
 ]
 
-class AnimationView extends Component {
-    state = {
-        fontType: 'animated bounceInRight'
-    }
-    changeType = v => {
-        console.log(v)
-        this.setState({
-            fontType: `animated ${v}`
-        })
-    }
-    render() {
-        return (
-            <Layout className='animated fadeIn'>
-                <div>
-                    <CustomBreadcrumb arr={['其他', '动画']}></CustomBreadcrumb>
-                </div>
-                <div className='base-style'>
-                    <h3>何时使用</h3>
-                    <Divider />
-                    <p>当页面需要动态效果时。</p>
-                </div>
-                <Row gutter={8} style={{ marginTop: '3rem' }}>
-                    <Col span={10}>
-                        <Tabs type='card' tabPosition='left'>
-                            <TabPane tab='进场' key='1'>
-                                {typeIn.map((v, i) => (
-                                    <Button
-                                        type='primary'
-                                        size='small'
-                                        key={i}
-                                        onClick={this.changeType.bind(this, v)}
-                                        ghost>
-                                        {v}
-                                    </Button>
-                                ))}
-                            </TabPane>
-                            <TabPane tab='退场' key='2'>
-                                {typeOut.map((v, i) => (
-                                    <Button
-                                        type='primary'
-                                        size='small'
-                                        key={i}
-                                        onClick={this.changeType.bind(this, v)}
-                                        ghost>
-                                        {v}
-                                    </Button>
-                                ))}
-                            </TabPane>
-                            <TabPane tab='其它' key='3'>
-                                {typeOther.map((v, i) => (
-                                    <Button
-                                        type='primary'
-                                        size='small'
-                                        key={i}
-                                        onClick={this.changeType.bind(this, v)}
-                                        ghost>
-                                        {v}
-                                    </Button>
-                                ))}
-                            </TabPane>
-                        </Tabs>
-                    </Col>
-                    <Col span={14}>
-                        <div
-                            style={{ fontSize: '4.8rem', textAlign: 'center', padding: '2rem' }}
-                            className={this.state.fontType}>
-                            Animate.css
-                        </div>
-                    </Col>
-                </Row>
-            </Layout>
-        )
-    }
+const AnimationView = () => {
+    const [fontType, setFontType] = useState('animated bounceInRight')
+    return (
+        <Layout className='animated fadeIn'>
+            <div>
+                <CustomBreadcrumb arr={['其他', '动画']}></CustomBreadcrumb>
+            </div>
+            <div className='base-style'>
+                <h3>何时使用</h3>
+                <Divider />
+                <p>当页面需要动态效果时。</p>
+            </div>
+            <Row gutter={8} style={{ marginTop: '3rem' }}>
+                <Col span={10}>
+                    <Tabs type='card' tabPosition='left'>
+                        <TabPane tab='进场' key='1'>
+                            {typeIn.map((v, i) => (
+                                <Button
+                                    type='primary'
+                                    size='small'
+                                    key={i}
+                                    onClick={() => setFontType(`animated ${v}`)}
+                                    ghost>
+                                    {v}
+                                </Button>
+                            ))}
+                        </TabPane>
+                        <TabPane tab='退场' key='2'>
+                            {typeOut.map((v, i) => (
+                                <Button
+                                    type='primary'
+                                    size='small'
+                                    key={i}
+                                    onClick={() => setFontType(`animated ${v}`)}
+                                    ghost>
+                                    {v}
+                                </Button>
+                            ))}
+                        </TabPane>
+                        <TabPane tab='其它' key='3'>
+                            {typeOther.map((v, i) => (
+                                <Button
+                                    type='primary'
+                                    size='small'
+                                    key={i}
+                                    onClick={() => setFontType(`animated ${v}`)}
+                                    ghost>
+                                    {v}
+                                </Button>
+                            ))}
+                        </TabPane>
+                    </Tabs>
+                </Col>
+                <Col span={14}>
+                    <div style={{ fontSize: '4.8rem', textAlign: 'center', padding: '2rem' }} className={fontType}>
+                        Animate.css
+                    </div>
+                </Col>
+            </Row>
+        </Layout>
+    )
 }
 
 export default AnimationView
