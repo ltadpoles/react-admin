ee14955f7d8b3365f5c5095aca7f52fda50e5afb
tadpole <ltadpole@163.com>
2019-11-11 17:20:46 +0800

    hook

--- src/views/ShowView/Tree/Tree.jsx
+++ src/views/ShowView/Tree/Tree.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import CustomBreadcrumb from '@/components/CustomBreadcrumb'
 import { Layout, Divider, Row, Col, Tree, Input, Icon } from 'antd'
 
@@ -106,33 +106,20 @@ const getParentKey = (key, tree) => {
     return parentKey
 }
 
-class TreeView extends Component {
-    state = {
-        expandedKeys: ['0-0-0', '0-0-1'],
-        autoExpandParent: true,
-        checkedKeys: ['0-0-0'],
-        selectedKeys: [],
-        searchValue: '',
-        gData
+const TreeView = () => {
+    const [expandedKeys, setexpandedKeys] = useState(['0-0-0', '0-0-1'])
+    const [autoExpandParent, setautoExpandParent] = useState(true)
+    const [checkedKeys, setcheckedKeys] = useState(['0-0-0'])
+    const [selectedKeys, setselectedKeys] = useState([])
+    const [searchValue, setsearchValue] = useState('')
+    const [gData, setgData] = useState([])
+
+    const onExpand = expandedKeys => {
+        setexpandedKeys(expandedKeys)
+        setautoExpandParent(false)
     }
 
-    onDragEnter = info => {
-        console.log(info)
-        // expandedKeys 需要受控时设置
-        this.setState({
-            expandedKeys: info.expandedKeys
-        })
-    }
-
-    onExpand = expandedKeys => {
-        console.log('onExpand', expandedKeys)
-        this.setState({
-            expandedKeys,
-            autoExpandParent: false
-        })
-    }
-
-    onDrop = info => {
+    const onDrop = info => {
         console.log(info)
         const dropKey = info.node.props.eventKey
         const dragKey = info.dragNode.props.eventKey
@@ -149,7 +136,7 @@ class TreeView extends Component {
                 }
             })
         }
-        const data = [...this.state.gData]
+        const data = [gData]
 
         // Find dragObject
         let dragObj
@@ -188,35 +175,30 @@ class TreeView extends Component {
                 ar.splice(i + 1, 0, dragObj)
             }
         }
-
-        this.setState({
-            gData: data
-        })
+        setgData(data)
     }
 
-    onCheck = checkedKeys => {
-        console.log('onCheck', checkedKeys)
-        this.setState({ checkedKeys })
+    const onCheck = checkedKeys => {
+        setcheckedKeys(checkedKeys)
     }
 
-    onSelect = (selectedKeys, info) => {
-        console.log('onSelect', info)
-        this.setState({ selectedKeys })
+    const onSelect = (selectedKeys, info) => {
+        setselectedKeys(selectedKeys)
     }
 
-    renderTreeNodes = data =>
+    const renderTreeNodes = data =>
         data.map(item => {
             if (item.children) {
                 return (
                     <TreeNode title={item.title} key={item.key} dataRef={item}>
-                        {this.renderTreeNodes(item.children)}
+                        {renderTreeNodes(item.children)}
                     </TreeNode>
                 )
             }
             return <TreeNode key={item.key} {...item} />
         })
 
-    onChange = e => {
+    const onChange = e => {
         const { value } = e.target
         const expandedKeys = dataList
             .map(item => {
@@ -226,130 +208,125 @@ class TreeView extends Component {
                 return null
             })
             .filter((item, i, self) => item && self.indexOf(item) === i)
-        this.setState({
-            expandedKeys,
-            searchValue: value,
-            autoExpandParent: true
-        })
+        setexpandedKeys(expandedKeys)
+        setsearchValue(value)
+        setautoExpandParent(true)
     }
 
-    render() {
-        const { searchValue, expandedKeys, autoExpandParent } = this.state
-        const loopDrop = data =>
-            data.map(item => {
-                if (item.children && item.children.length) {
-                    return (
-                        <TreeNode key={item.key} title={item.title}>
-                            {loop(item.children)}
-                        </TreeNode>
-                    )
-                }
-                return <TreeNode key={item.key} title={item.title} />
-            })
-        const loop = data =>
-            data.map(item => {
-                const index = item.title.indexOf(searchValue)
-                const beforeStr = item.title.substr(0, index)
-                const afterStr = item.title.substr(index + searchValue.length)
-                const title =
-                    index > -1 ? (
-                        <span>
-                            {beforeStr}
-                            <span style={{ color: '#f50' }}>{searchValue}</span>
-                            {afterStr}
-                        </span>
-                    ) : (
-                        <span>{item.title}</span>
-                    )
-                if (item.children) {
-                    return (
-                        <TreeNode key={item.key} title={title}>
-                            {loop(item.children)}
-                        </TreeNode>
-                    )
-                }
-                return <TreeNode key={item.key} title={title} />
-            })
-        return (
-            <Layout className='animated fadeIn'>
-                <div>
-                    <CustomBreadcrumb arr={['展示', '树形控件']}></CustomBreadcrumb>
-                </div>
-                <div className='base-style'>
-                    <h3>何时使用</h3>
-                    <Divider />
-                    <p>
-                        文件夹、组织架构、生物分类、国家地区等等，世间万物的大多数结构都是树形结构。使用 树控件
-                        可以完整展现其中的层级关系，并具有展开收起选择等交互功能。
-                    </p>
-                </div>
-                <Row gutter={8}>
-                    <Col span={12}>
-                        <div className='base-style'>
-                            <Divider orientation='left'>基础用法</Divider>
-                            <Tree
-                                checkable
-                                showIcon
-                                defaultExpandedKeys={['0-0-0', '0-0-1']}
-                                defaultSelectedKeys={['0-0-0', '0-0-1']}
-                                defaultCheckedKeys={['0-0-0', '0-0-1']}
-                                onSelect={this.onSelect}
-                                onCheck={this.onCheck}>
-                                <TreeNode title='parent 1' key='0-0'>
-                                    <TreeNode icon={<Icon type='meh-o' />} title='parent 1-0' key='0-0-0' disabled>
-                                        <TreeNode title='leaf' key='0-0-0-0' disableCheckbox />
-                                        <TreeNode title='leaf' key='0-0-0-1' />
-                                    </TreeNode>
-                                    <TreeNode title='parent 1-1' key='0-0-1' icon={<Icon type='meh-o' />}>
-                                        <TreeNode title={<span style={{ color: '#1890ff' }}>sss</span>} key='0-0-1-0' />
-                                    </TreeNode>
+    const loopDrop = data =>
+        data.map(item => {
+            if (item.children && item.children.length) {
+                return (
+                    <TreeNode key={item.key} title={item.title}>
+                        {loops(item.children)}
+                    </TreeNode>
+                )
+            }
+            return <TreeNode key={item.key} title={item.title} />
+        })
+    const loops = data =>
+        data.map(item => {
+            const index = item.title.indexOf(searchValue)
+            const beforeStr = item.title.substr(0, index)
+            const afterStr = item.title.substr(index + searchValue.length)
+            const title =
+                index > -1 ? (
+                    <span>
+                        {beforeStr}
+                        <span style={{ color: '#f50' }}>{searchValue}</span>
+                        {afterStr}
+                    </span>
+                ) : (
+                    <span>{item.title}</span>
+                )
+            if (item.children) {
+                return (
+                    <TreeNode key={item.key} title={title}>
+                        {loops(item.children)}
+                    </TreeNode>
+                )
+            }
+            return <TreeNode key={item.key} title={title} />
+        })
+    return (
+        <Layout className='animated fadeIn'>
+            <div>
+                <CustomBreadcrumb arr={['展示', '树形控件']}></CustomBreadcrumb>
+            </div>
+            <div className='base-style'>
+                <h3>何时使用</h3>
+                <Divider />
+                <p>
+                    文件夹、组织架构、生物分类、国家地区等等，世间万物的大多数结构都是树形结构。使用 树控件
+                    可以完整展现其中的层级关系，并具有展开收起选择等交互功能。
+                </p>
+            </div>
+            <Row gutter={8}>
+                <Col span={12}>
+                    <div className='base-style'>
+                        <Divider orientation='left'>基础用法</Divider>
+                        <Tree
+                            checkable
+                            showIcon
+                            defaultExpandedKeys={['0-0-0', '0-0-1']}
+                            defaultSelectedKeys={['0-0-0', '0-0-1']}
+                            defaultCheckedKeys={['0-0-0', '0-0-1']}
+                            onSelect={onSelect}
+                            onCheck={onCheck}>
+                            <TreeNode title='parent 1' key='0-0'>
+                                <TreeNode icon={<Icon type='meh-o' />} title='parent 1-0' key='0-0-0' disabled>
+                                    <TreeNode title='leaf' key='0-0-0-0' disableCheckbox />
+                                    <TreeNode title='leaf' key='0-0-0-1' />
                                 </TreeNode>
-                            </Tree>
-                        </div>
-                        <div className='base-style'>
-                            <Divider orientation='left'>可搜索</Divider>
-                            <Search style={{ marginBottom: 8 }} placeholder='Search' onChange={this.onChange} />
-                            <Tree
-                                showLine
-                                onExpand={this.onExpand}
-                                expandedKeys={expandedKeys}
-                                autoExpandParent={autoExpandParent}>
-                                {loop(gData)}
-                            </Tree>
-                        </div>
-                    </Col>
-                    <Col span={12}>
-                        <div className='base-style'>
-                            <Divider orientation='left'>受控控件</Divider>
-                            <Tree
-                                checkable
-                                onExpand={this.onExpand}
-                                expandedKeys={this.state.expandedKeys}
-                                autoExpandParent={this.state.autoExpandParent}
-                                onCheck={this.onCheck}
-                                checkedKeys={this.state.checkedKeys}
-                                onSelect={this.onSelect}
-                                selectedKeys={this.state.selectedKeys}>
-                                {this.renderTreeNodes(treeData)}
-                            </Tree>
-                        </div>
-                        <div className='base-style'>
-                            <Divider orientation='left'>可拖拽</Divider>
-                            <Tree
-                                className='draggable-tree'
-                                defaultExpandedKeys={this.state.expandedKeys}
-                                draggable
-                                blockNode
-                                onDragEnter={this.onDragEnter}
-                                onDrop={this.onDrop}>
-                                {loopDrop(this.state.gData)}
-                            </Tree>
-                        </div>
-                    </Col>
-                </Row>
-            </Layout>
-        )
-    }
+                                <TreeNode title='parent 1-1' key='0-0-1' icon={<Icon type='meh-o' />}>
+                                    <TreeNode title={<span style={{ color: '#1890ff' }}>sss</span>} key='0-0-1-0' />
+                                </TreeNode>
+                            </TreeNode>
+                        </Tree>
+                    </div>
+                    <div className='base-style'>
+                        <Divider orientation='left'>可搜索</Divider>
+                        <Search style={{ marginBottom: 8 }} placeholder='Search' onChange={onChange} />
+                        <Tree
+                            showLine
+                            onExpand={onExpand}
+                            expandedKeys={expandedKeys}
+                            autoExpandParent={autoExpandParent}>
+                            {loops(gData)}
+                        </Tree>
+                    </div>
+                </Col>
+                <Col span={12}>
+                    <div className='base-style'>
+                        <Divider orientation='left'>受控控件</Divider>
+                        <Tree
+                            checkable
+                            onExpand={onExpand}
+                            expandedKeys={expandedKeys}
+                            autoExpandParent={autoExpandParent}
+                            onCheck={onCheck}
+                            checkedKeys={checkedKeys}
+                            onSelect={onSelect}
+                            selectedKeys={selectedKeys}>
+                            {renderTreeNodes(treeData)}
+                        </Tree>
+                    </div>
+                    <div className='base-style'>
+                        <Divider orientation='left'>可拖拽</Divider>
+                        <Tree
+                            className='draggable-tree'
+                            defaultExpandedKeys={expandedKeys}
+                            draggable
+                            blockNode
+                            onDragEnter={info => setexpandedKeys(info.expandedKeys)}
+                            onDrop={onDrop}>
+                            {loopDrop(gData)}
+                        </Tree>
+                    </div>
+                </Col>
+            </Row>
+        </Layout>
+    )
 }
 
 export default TreeView
