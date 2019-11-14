368595b5698b03584b00b3861da77440e43c6186
tadpole <ltadpole@163.com>
2019-11-14 17:24:31 +0800

    hook

--- src/containers/AppAside.jsx
+++ src/containers/AppAside.jsx
@@ -1,24 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Layout, Icon } from 'antd'
 import CustomMenu from '@/components/CustomMenu'
 
 const { Sider } = Layout
 
-class AppAside extends Component {
-    render() {
-        let { menuToggle, menu } = this.props
-        return (
-            <Sider className='aside' collapsed={menuToggle}>
-                <div className='logo'>
-                    <a rel='noopener noreferrer' href='https://github.com/ltadpoles' target='_blank'>
-                        <Icon type='github' style={{ fontSize: '3.8rem', color: '#fff' }} />
-                    </a>
-                </div>
-                <CustomMenu menu={menu}></CustomMenu>
-            </Sider>
-        )
-    }
+const AppAside = props => {
+    let { menuToggle, menu } = props
+    return (
+        <Sider className='aside' collapsed={menuToggle}>
+            <div className='logo'>
+                <a rel='noopener noreferrer' href='https://github.com/ltadpoles' target='_blank'>
+                    <Icon type='github' style={{ fontSize: '3.8rem', color: '#fff' }} />
+                </a>
+            </div>
+            <CustomMenu menu={menu}></CustomMenu>
+        </Sider>
+    )
 }
 
 AppAside.propTypes = {
--- src/containers/DefaultLayout.jsx
+++ src/containers/DefaultLayout.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { Layout, BackTop, message } from 'antd'
@@ -6,7 +6,7 @@ import routes from '@/routes'
 import { menuToggleAction } from '@/store/actionCreators'
 import echarts from 'echarts/lib/echarts'
 import avatar from '@/assets/images/user.jpg'
-import menu from './menu'
+import menus from './menu'
 import '@/style/layout.scss'
 
 import AppHeader from './AppHeader.jsx'
@@ -15,49 +15,43 @@ import AppFooter from './AppFooter.jsx'
 
 const { Content } = Layout
 
-class DefaultLayout extends Component {
-    state = {
-        avatar,
-        show: true,
-        menu: []
+const getMenu = menu => {
+    let newMenu,
+        auth = JSON.parse(localStorage.getItem('user')).auth
+    if (!auth) {
+        return menu
+    } else {
+        newMenu = menu.filter(res => res.auth && res.auth.indexOf(auth) !== -1)
+        return newMenu
     }
+}
 
-    isLogin = () => {
+const DefaultLayout = props => {
+    const [menu] = useState(prevState => {
         if (!localStorage.getItem('user')) {
-            this.props.history.push('/login')
+            props.history.push('/login')
+            return []
         } else {
-            this.setState({
-                menu: this.getMenu(menu)
-            })
+            return getMenu(menus)
         }
-    }
+    })
 
-    loginOut = () => {
+    let { menuClick, menuToggle } = props
+    let { auth } = JSON.parse(localStorage.getItem('user')) ? JSON.parse(localStorage.getItem('user')) : ''
+
+    const loginOut = () => {
         localStorage.clear()
-        this.props.history.push('/login')
+        props.history.push('/login')
         message.success('登出成功!')
     }
-    getMenu = menu => {
-        let newMenu,
-            auth = JSON.parse(localStorage.getItem('user')).auth
-        if (!auth) {
-            return menu
-        } else {
-            newMenu = menu.filter(res => res.auth && res.auth.indexOf(auth) !== -1)
-            return newMenu
-        }
-    }
 
-    componentDidMount() {
-        this.isLogin()
-    }
-
-    componentDidUpdate() {
-        let { pathname } = this.props.location
+    useEffect(() => {
+        let { pathname } = props.location
+        let timer
 
         // 菜单收缩展开时 echarts 图表的自适应
         if (pathname === '/' || pathname === '/index') {
-            this.timer = setTimeout(() => {
+            timer = setTimeout(() => {
                 echarts.init(document.getElementById('bar')).resize()
                 echarts.init(document.getElementById('line')).resize()
                 echarts.init(document.getElementById('pie')).resize()
@@ -65,57 +59,46 @@ class DefaultLayout extends Component {
                 echarts.init(document.getElementById('scatter')).resize()
             }, 500)
         } else {
-            this.timer = null
+            timer = null
         }
-    }
+        return () => {
+            timer && clearTimeout(timer)
+        }
+    })
 
-    componentWillUnmount() {
-        this.timer && clearTimeout(this.timer)
-    }
-
-    render() {
-        let { menuClick, menuToggle } = this.props
-        let { auth } = JSON.parse(localStorage.getItem('user')) ? JSON.parse(localStorage.getItem('user')) : ''
-        return (
-            <Layout className='app'>
-                <BackTop />
-                <AppAside menuToggle={menuToggle} menu={this.state.menu} />
-                <Layout style={{ marginLeft: menuToggle ? '80px' : '200px', minHeight: '100vh' }}>
-                    <AppHeader
-                        menuToggle={menuToggle}
-                        menuClick={menuClick}
-                        avatar={this.state.avatar}
-                        show={this.state.show}
-                        loginOut={this.loginOut}
-                    />
-                    <Content className='content'>
-                        <Switch>
-                            {routes.map(item => {
-                                return (
-                                    <Route
-                                        key={item.path}
-                                        path={item.path}
-                                        exact={item.exact}
-                                        render={props =>
-                                            !auth ? (
-                                                <item.component {...props} />
-                                            ) : item.auth && item.auth.indexOf(auth) !== -1 ? (
-                                                <item.component {...props} />
-                                            ) : (
-                                                // 这里也可以跳转到 403 页面
-                                                <Redirect to='/404' {...props} />
-                                            )
-                                        }></Route>
-                                )
-                            })}
-                            <Redirect to='/404' />
-                        </Switch>
-                    </Content>
-                    <AppFooter />
-                </Layout>
+    return (
+        <Layout className='app'>
+            <BackTop />
+            <AppAside menuToggle={menuToggle} menu={menu} />
+            <Layout style={{ marginLeft: menuToggle ? '80px' : '200px', minHeight: '100vh' }}>
+                <AppHeader menuToggle={menuToggle} menuClick={menuClick} avatar={avatar} loginOut={loginOut} />
+                <Content className='content'>
+                    <Switch>
+                        {routes.map(item => {
+                            return (
+                                <Route
+                                    key={item.path}
+                                    path={item.path}
+                                    exact={item.exact}
+                                    render={props =>
+                                        !auth ? (
+                                            <item.component {...props} />
+                                        ) : item.auth && item.auth.indexOf(auth) !== -1 ? (
+                                            <item.component {...props} />
+                                        ) : (
+                                            // 这里也可以跳转到 403 页面
+                                            <Redirect to='/404' {...props} />
+                                        )
+                                    }></Route>
+                            )
+                        })}
+                        <Redirect to='/404' />
+                    </Switch>
+                </Content>
+                <AppFooter />
             </Layout>
-        )
-    }
+        </Layout>
+    )
 }
 
 const stateToProp = state => ({
