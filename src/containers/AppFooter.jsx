e582240cfa4b5b6813b59049ef02d9dd3798fb7f
yi.he <yi.he@shopee.com>
2023-04-12 19:43:21 +0800

    update

--- src/containers/AppFooter.jsx
+++ src/containers/AppFooter.jsx
@@ -3,4 +3,4 @@ import { Layout } from 'antd'
 
 const { Footer } = Layout
 
-export default () => <Footer className='footer'>React Admin &copy;2019 Created By 游荡de蝌蚪</Footer>
+export default () => <Footer className='footer'>Work Flow Service &copy;2023 Created By Inhouse</Footer>
